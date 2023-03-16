0d07a9bd19a03ab5820722ad13015bebca1640e2
Adam <adamchatila2020@gmail.com>
2023-03-16 15:51:19 +0100

    changing calander style

--- src/components/calander/CalendarDefault.js
+++ src/components/calander/CalendarDefault.js
@@ -26,17 +26,16 @@ const CalendarDefault = () => {
     <div>
       <h1 className='text-white font-normal pl-9 pt-9 text-4xl'>kalender</h1>
       <div>
-      {activities.map(activity => (
-  <div className="flex flex-wrap justify-center pt-[30px] pl-4 pr-4">
-    <Link  className="bg-white h-[107px] mb-5  w-[356px] rounded-lg shadow-md">
-      <div className='flex flex-col justify-center'>
-        <h2 className="text-black font-normal text-center text-4xl">{activity.name}</h2>
-        <p className="text-black font-normal text-center  text-lg">{activity.weekday} {activity.time}</p>
-        <p className="text-black font-normal text-center text-lg">Age Restriction: {activity.ageRestriction.min} - {activity.ageRestriction.max}</p>
+        {activities.map(activity => (
+        <div className="flex flex-wrap justify-center pt-[30px] pl-4 pr-4">
+        <Link  className="bg-white h-[107px] mb-5  w-[356px] rounded-lg shadow-md">
+          <div className='flex flex-col justify-center'>
+          <h2 className="text-black font-normal text-center text-4xl">{activity.name}</h2>
+          <p className="text-black font-normal text-center  text-lg">{activity.weekday} {activity.time}</p>
+          </div>
+        </Link>
       </div>
-    </Link>
-  </div>
-))}
+        ))}
       </div>
       <NavBar/>
     </div>
